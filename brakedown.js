1efb04a68655e14f96712166bf95ec7cbd1bc0c1
Alex Yu <alexyu@umd.edu>
2016-10-22 01:51:35 -0400

    added additional logic

--- brakedown.js
+++ brakedown.js
@@ -16,23 +16,48 @@ exports.initGame = function(sio, socket){
 
 
 // Game Logic
-var xPos, yPos, velocity;
 
-var speed = 1;
+//course variables
+const bound = 500; //distance from center that counts as out of bounds
+const track_length = 10000;
 
-var numPlayers = 0.0;
+//game states
+const GAME_IN_PROGRESS = 1;
+const GAME_OVER_WON = 2;
+const GAME_OVER_LOST = 3;
+
+var xPos=0.0, yPos=0.0, velocity=0.0; // velocity = left/right speed
+
+var forwardSpeed = 1;
+
+var numPlayers = 0;
+
+var gameState =
+
 
 function update(deltaTime){
-  //update position
-  //check for collisions and update game state as necessary
+  updatePosition(deltaTime);
+  updateGameStatus(checkCollisions());
 }
 
 function updatePosition(deltaTime){
   xPos += velocity*deltaTime;
-  yPos += deltaTime*speed;
+  yPos += deltaTime*forwardSpeed;
 }
 
-function updateVelocity(newVelocity){ //adds a player's wheel setting to overall
-  velocity += newVelocity/numPlayers;
-  
+function updateVelocity(newVelocity){ //Adds a player's wheel setting to a moving average, asynchronous
+  velocity -= velocity / numPlayers;  //Call this once per update interval for each user
+  velocity += newVelocity / numPlayers;
+}
+
+function updateGameStatus(collisionOccurred){
+  if(yPos > track_length){
+    gameState = GAME_OVER_WON;
+  } else if(collisionOccurred){
+    gameState = GAME_OVER_LOST;
+  }
+}
+
+function checkCollisions(){
+
 }
