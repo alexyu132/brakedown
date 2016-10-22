bba08a8fb013c8a3d67d3ac0b5e497841ba3d3cd
Anthony Li <antli.inbox@gmail.com>
2016-10-22 00:43:00 -0400

    scaffolding

---
+++ brakedown.js
@@ -0,0 +1,15 @@
+/**
+ * This function is called by index.js to initialize a new game instance.
+ *
+ * @param sio The Socket.IO library
+ * @param socket The socket object for the connected client.
+ */
+exports.initGame = function(sio, socket){
+    io = sio;
+    gameSocket = socket;
+    gameSocket.emit('connected', { message: "You are connected!" });
+
+    // Host Events
+    //gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+
+}
