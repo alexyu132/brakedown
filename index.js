b22237925dfa134ff631b4e25a52af7d36520623
Anthony Li <antli.inbox@gmail.com>
2016-10-22 08:58:06 -0400

    Undo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var brakedown = require('./brakedown.js');
 
 
 // Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(process.env.PORT || 80);
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
