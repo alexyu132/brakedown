e8e0bb4757aa91abbb32ffab3d044211f970d61e
Ji Hyuk Bae <jihyuk.b@gmail.com>
2016-10-22 07:22:37 -0400

    no change

--- public/app.js
+++ public/app.js
@@ -105,8 +105,9 @@ jQuery(function($) {
     IO.init();
 
 }($));
+
 window.addEventListener("load", canvasApp, false);
-function canvasApp(){	
+function canvasApp(){
 	window.requestAnimFrame = (function(callback) {
         	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || 		window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
         	function(callback) {
@@ -166,9 +167,9 @@ function canvasApp(){
 		ctx.beginPath();
 		ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI,true);
 		ctx.stroke();
-		
+
 	}
-	
+
 	var ctx = canvas.getContext("2d");
 	drawRoad();
 	drawCar();
